c36ae02a9d65a8a49f085a72ce74b236472d0a19
eduair94 <49965850+eduair94@users.noreply.github.com>
2026-03-24 00:18:07 -0300

    fix: music bot

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,9 @@ module.exports = {
   apps: [{
     name: 'music-bot',
     script: './dist/index.js',
+    cwd: __dirname,
     instances: 1,
     autorestart: true,
+    node_args: '-r dotenv/config',
   }]
 };
